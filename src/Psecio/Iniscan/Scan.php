cb947e541a14e8bd92d91cfe86c93247be749ccb
enygma <enygma@phpdeveloper.org>
2013-10-25 10:04:12 -0500

    fleshing out the Scan class more

--- src/Psecio/Iniscan/Scan.php
+++ src/Psecio/Iniscan/Scan.php
@@ -4,5 +4,52 @@ namespace Psecio\Iniscan;
 
 class Scan
 {
-	
+	private $path;
+	private $rules = array(
+		'session'
+	);
+
+	public function __construct($path)
+	{
+		$this->setPath($path);
+	}
+
+	public function setPath($path)
+	{
+		if (!is_file($path)) {
+			throw new \InvalidArgumentException('Path '.$path.' invalid');
+		}
+		$this->path = realpath($path);
+	}
+	public function getPath()
+	{
+		return $this->path;
+	}
+
+	public function getRules()
+	{
+		return $this->rules;
+	}
+
+	public function execute()
+	{
+		$path = $this->getPath();
+		$ini = parse_ini_file($path, true);
+
+		// pull in the rule configuration
+		$rules = json_decode(file_get_contents(__DIR__.'/rules.json'));
+
+		$ruleList = array();
+		foreach ($rules->rules as $index => $ruleSet) {
+			foreach ($ruleSet as $type => $rule) {
+				// make a rule
+				$rule = new \Psecio\Iniscan\Rule($rule);
+
+				// execute its test
+				$rule->evaluate($ini);
+				$ruleList[] = $rule;
+			}
+		}
+		return $ruleList;
+	}
 }
