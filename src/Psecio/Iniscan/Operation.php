69c9484ff8113ace2747ab4b41c786a84ab317d1
enygma <enygma@phpdeveloper.org>
2013-10-26 12:10:39 -0500

    adding Section to the Operation

--- src/Psecio/Iniscan/Operation.php
+++ src/Psecio/Iniscan/Operation.php
@@ -4,11 +4,27 @@ namespace Psecio\Iniscan;
 
 abstract class Operation
 {
+	public $section;
+
+	public function __construct($section)
+	{
+		$this->setSection($section);
+	}
+
+	public function setSection($section)
+	{
+		$this->section = $section;
+	}
+	public function getSection()
+	{
+		return $this->section;
+	}
+
 	public abstract function execute($key, $value, $ini);
 
 	public function findValue($path, $ini)
 	{
-		list($section, $path) = explode('|', $path);
+		$section = $this->getSection();
 		if (!array_key_exists($section, $ini)) {
 			throw new \InvalidArgumentException('Unknown section '.$section);
 		}
