7f432df415586481c7d6ec64ee61884936192ba8
enygma <enygma@phpdeveloper.org>
2013-10-28 15:28:17 -0500

    adding docblocks to DisableCliFunctions check

--- src/Psecio/Iniscan/Rule/DisableCliFunctions.php
+++ src/Psecio/Iniscan/Rule/DisableCliFunctions.php
@@ -2,17 +2,31 @@
 
 namespace Psecio\Iniscan\Rule;
 
+/**
+ * Custom operation - Checks to see if the system (cli) functions
+ * 	are disabled.
+ */
 class DisableCliFunctions extends \Psecio\Iniscan\Rule
 {
+	/**
+	 * Functions to disable
+	 * @var array
+	 */
 	private $functions = array(
 		'exec', 'passthru', 'shell_exec', 'system', 
 		'proc_open', 'popen', 'curl_exec', 'curl_multi_exec'
 	);
 
+	/**
+	 * Evaluate the operation
+	 * 
+	 * @param array $ini 
+	 * @return boolean Pass/fail result
+	 */
 	public function evaluate($ini)
 	{
 		if (isset($ini['PHP']['disable_functions'])) {
-			
+
 			$functions = explode(',', $ini['PHP']['disable_functions']);
 			foreach ($functions as $function) {
 				$search = array_search($function, $this->functions);
@@ -24,8 +38,10 @@ class DisableCliFunctions extends \Psecio\Iniscan\Rule
 		if (!empty($this->functions)) {
 			$this->setDescription('Methods still enabled - '.implode(', ', $this->functions));
 			$this->fail();
+			return false;
 		} else {
 			$this->pass();
+			return true;
 		}
 	}
 }
