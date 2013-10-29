ce8c4102e8f10c6e1d2465825a000435bc625ed6
Chris Cornutt <ccornutt@ccornutt.local>
2013-10-29 15:09:26 -0500

    adding return statements to max post size rule

--- src/Psecio/Iniscan/Rule/MaximumPostSize.php
+++ src/Psecio/Iniscan/Rule/MaximumPostSize.php
@@ -10,16 +10,18 @@ class MaximumPostSize extends \Psecio\Iniscan\Rule
 	private $maxPost = 8;
 
 	public function evaluate($ini)
-	{	
+	{
 		$postSize = $ini['PHP']['post_max_size'];
-		
+
 		// find the number(s)
 		preg_match('/[0-9]+/', $postSize, $match);
 		if (isset($match[0]) && $match[0] > $this->maxPost) {
 			$this->setDescription('Unless neccessary, a maximum post size of '.$postSize.' is too large');
 			$this->fail();
+			return false;
 		} else {
 			$this->pass();
+			return true;
 		}
 	}
 }
