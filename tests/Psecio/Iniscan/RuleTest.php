52c12e0507e257046ddd40fedab8468a1e71e8bc
Chris Cornutt <ccornutt@ccornutt.local>
2013-10-31 16:24:59 -0500

    adding tests for Rules

---
+++ tests/Psecio/Iniscan/RuleTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Psecio\Iniscan;
+
+class RuleTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test that the configuration is set correctly on init
+     *
+     * @covers \Psecio\Iniscan\Rule::__construct
+     */
+    public function testConfigOnInit()
+    {
+        $name = 'test rule';
+        $config = array('name' => $name);
+
+        $rule = new Rule($config, 'testsection');
+        $this->assertEquals($rule->getName(), $name);
+    }
+
+    /**
+     * Test that the rule fails correctly
+     *
+     * @covers \Psecio\Iniscan\Rule::fail
+     */
+    public function testFailStatus()
+    {
+        $rule = new Rule(array(), 'testsection');
+        $rule->fail();
+
+        $this->assertFalse($rule->getStatus());
+    }
+
+    /**
+     * Test that the rule passes correctly
+     *
+     * @covers \Psecio\Iniscan\Rule::pass
+     */
+    public function testPassStatus()
+    {
+        $rule = new Rule(array(), 'testsection');
+        $rule->pass();
+
+        $this->assertTrue($rule->getStatus());
+    }
+
+    /**
+     * Test that the "test" is set correctly
+     *
+     * @covers \Psecio\Iniscan\Rule::setTest
+     * @covers \Psecio\Iniscan\Rule::getTest
+     */
+    public function testGetSetTest()
+    {
+        $test = array(
+            'key' => 'foo'
+        );
+        $rule = new Rule(array(), 'testsection');
+        $rule->setTest($test);
+
+        $this->assertEquals($rule->getTest(), (object)$test);
+    }
+
+    /**
+     * Test that the description is set correctly
+     *
+     * @covers \Psecio\Iniscan\Rule::setDescription
+     * @covers \Psecio\Iniscan\Rule::getDescription
+     */
+    public function testGetSetDescription()
+    {
+        $desc = 'this is the rule description';
+        $rule = new Rule(array(), 'testsection');
+
+        $rule->setDescription($desc);
+        $this->assertEquals($rule->getDescription(), $desc);
+    }
+
+    /**
+     * Test that the section is set correctly
+     *
+     * @covers \Psecio\Iniscan\Rule::getSection
+     * @covers \Psecio\Iniscan\Rule::setSection
+     */
+    public function testGetSection()
+    {
+        $rule = new Rule(array(), 'testing123');
+        $this->assertEquals('testing123', $rule->getSection());
+
+        $rule->setSection('foobar');
+        $this->assertEquals('foobar', $rule->getSection());
+    }
+}
