4278c9271fa33f08e2e50947f3e3a2c299741f00
Chris Cornutt <ccornutt@ccornutt.local>
2013-11-01 10:43:37 -0500

    updating Scan command to use new output handling

--- src/Psecio/Iniscan/Command/ScanCommand.php
+++ src/Psecio/Iniscan/Command/ScanCommand.php
@@ -24,7 +24,7 @@ class ScanCommand extends Command
 
     /**
      * Execute the "scan" command
-     * 
+     *
      * @param  InputInterface  $input  Input object
      * @param  OutputInterface $output Output object
      * @return null
@@ -46,48 +46,14 @@ class ScanCommand extends Command
         $scan = new \Psecio\Iniscan\Scan($path);
         $results = $scan->execute();
 
-        // loop through the results and output color coded
-        $output->writeLn("\nResults for ".$path.":\n".str_repeat('=', 12));
-        $output->writeLn(
-            str_pad("Status", 7, ' ').'| '
-            .str_pad("Severity", 9, ' ').'| '
-            .str_pad("Key", 25, ' ').'| Description'
+        $options = array(
+            'path' => $path,
+            'failOnly' => $failOnly
         );
-        $output->writeLn(str_repeat('-', 70));
-        $fail = 0;
-        $pass = 0;
 
-        foreach ($results as $result) {
-            if ($result->getStatus() === false) {
-                $fail++;
-                // if we failed, see how bad it is
-                $severity = $result->getLevel();
-                $color = ($severity == 'WARNING') ? 'yellow' : 'red';
-                $status = 'FAIL';
-            } else {
-                $pass++;
-                $status = 'PASS';
-                $color = 'green';
-            }
-            if ($failOnly === true && $status !== 'FAIL') {
-                continue;
-            }
-            $test = $result->getTest();
-            $test = (isset($test->key)) ? $test->key : '';
-
-            $output->writeLn(
-                '<fg='.$color.'>'
-                .str_pad($status, 7, ' ')
-                .'| '.str_pad($result->getLevel(), 9, ' ')
-                .'| '.str_pad($test, 25, ' ')
-                .'| '.$result->getDescription()
-                .'</fg='.$color.'>'
-                );
-        }
-        $output->writeLn("\n<info>".$pass." passing</info>\n<error>".$fail." failure(s)</error>");
-
-        return (count($fail) > 0) ? 1 : 0;
+        $outputHandler = new \Psecio\Iniscan\Command\Scan\Output\Console($output, $options);
+        return $outputHandler->render($results);
     }
 }
 
-?>  
+?>
