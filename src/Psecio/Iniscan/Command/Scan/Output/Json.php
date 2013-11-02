1707407eb409a46824068cfc254ec2fbff00c7d8
enygma <enygma@phpdeveloper.org>
2013-11-02 06:35:48 -0500

    adding json output for the "scan" command (Issue #5)

---
+++ src/Psecio/Iniscan/Command/Scan/Output/Json.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Psecio\Iniscan\Command\Scan\Output;
+
+class Json extends \Psecio\Iniscan\Command\Output
+{
+    public function render($results)
+    {
+        $output = $this->getOutput();
+        $path = $this->getOption('path');
+        $failOnly = $this->getOption('failOnly');
+
+        $resultValues = array();
+        foreach ($results as $result) {
+        	$resultValues[] = $result->values();
+        }
+
+        $output->writeLn(json_encode($resultValues));
+    }
+}
