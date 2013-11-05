e4784118599f22cefda9a2e7ce6a8474c366506e
Chris Cornutt <ccornutt@ccornutt.local>
2013-11-05 08:09:03 -0600

    removing one last unused "use" statement

--- src/Psecio/Iniscan/Command/ShowCommand.php
+++ src/Psecio/Iniscan/Command/ShowCommand.php
@@ -4,7 +4,6 @@ namespace Psecio\Iniscan\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
 class ShowCommand extends Command
