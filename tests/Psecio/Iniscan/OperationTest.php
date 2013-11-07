9a1d6b58e40f6477c6b8b933b90dd663578c8d9c
enygma <enygma@phpdeveloper.org>
2013-11-07 09:22:26 -0600

    adding more "casting" tests to Operation set

--- tests/Psecio/Iniscan/OperationTest.php
+++ tests/Psecio/Iniscan/OperationTest.php
@@ -6,12 +6,52 @@ require_once 'OperationStub.php';
 
 class OperationTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCastValue()
+	/**
+	 * Test the "casting" of a value to a consistent output
+	 * given a string
+	 * 
+	 * @covers \Psecio\Iniscan\Operation::castValue
+	 */
+    public function testCastValueString()
     {
         $input = 'Off';
         $operation = new OperationStub('test');
-
         $result = $operation->castValue($input);
+
         $this->assertEquals($result, '0');
+        $this->assertEquals(gettype($result), 'string');
+    }
+
+    /**
+	 * Test the "casting" of a value to a consistent output
+	 * given an integer
+	 * 
+	 * @covers \Psecio\Iniscan\Operation::castValue
+	 */
+    public function testCastValueInteger()
+    {
+    	$input = 1;
+        $operation = new OperationStub('test');
+        $result = $operation->castValue($input);
+
+        $this->assertEquals($result, '1');
+        $this->assertEquals(gettype($result), 'string');
+    }
+
+    /**
+     * Test the locating of a value in the given INI settings
+     * @covers \Psecio\Iniscan\Operation::findValue
+     */
+    public function testFndValue()
+    {
+    	$value = 'baz';
+    	$ini = array(
+    		'PHP' => array(
+    			'foo.bar' => $value
+    		)
+    	);
+    	$operation = new OperationStub('PHP');
+    	$result = $operation->findValue('foo.bar', $ini);
+    	$this->assertEquals($result, $value);
     }
 }
