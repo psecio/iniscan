f57a64f17c68ac8cda7c7c3c334192f6401384bd
enygma <enygma@phpdeveloper.org>
2013-11-08 20:07:40 -0500

    passing in the whole test instead of just the key to the operation

--- src/Psecio/Iniscan/Operation/OperationEquals.php
+++ src/Psecio/Iniscan/Operation/OperationEquals.php
@@ -8,13 +8,14 @@ class OperationEquals extends \Psecio\Iniscan\Operation
 	 * Execute the "equals" operation
 	 * 	If the value and the ini setting don't equal, returns false
 	 *
-	 * @param string $key Key name of setting
+	 * @param string $test Test information
 	 * @param string $value Value to match on
 	 * @param array $ini Current php.ini settings
 	 * @return boolean Pass/fail of operation
 	 */
-	public function execute($key, $value, $ini)
+	public function execute($test, $value, $ini)
 	{
+		$key = $test->key;
 		$found = $this->findValue($key, $ini);
 		if ($this->castValue($found) !== $this->castValue($value)) {
 			return false;
--- src/Psecio/Iniscan/Operation/OperationIsset.php
+++ src/Psecio/Iniscan/Operation/OperationIsset.php
@@ -8,13 +8,14 @@ class OperationIsset extends \Psecio\Iniscan\Operation
 	 * Execute the "is set" operation
 	 * 	If the value isn't set, return false
 	 * 
-	 * @param string $key Key name of setting
+	 * @param string $test Test information
 	 * @param string $value Value to match on
 	 * @param array $ini Current php.ini settings
 	 * @return boolean Pass/fail of operation
 	 */
-	public function execute($key, $value, $ini)
+	public function execute($test, $value, $ini)
 	{
+		$key = $test->key;
 		$found = $this->findValue($key, $ini);
 		if (empty($found)) {
 			return false;
