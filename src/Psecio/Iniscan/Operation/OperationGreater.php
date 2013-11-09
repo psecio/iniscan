38cb0e06c2b07c44df7d69f20bdcaa1b5ffb2cad
enygma <enygma@phpdeveloper.org>
2013-11-08 20:24:34 -0500

    adding a "greater than" operation

---
+++ src/Psecio/Iniscan/Operation/OperationGreater.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Psecio\Iniscan\Operation;
+
+class OperationGreater extends \Psecio\Iniscan\Operation
+{
+	/**
+	 * Execute the "greater than" operation
+	 * 	If the value and the ini setting isn't greater than the value
+	 *
+	 * @param string $key Key name of setting
+	 * @param string $value Value to match on
+	 * @param array $ini Current php.ini settings
+	 * @return boolean Pass/fail of operation
+	 */
+	public function execute($key, $value, $ini)
+	{
+		$found = $this->findValue($key, $ini);
+		if ($this->castValue($found) <= $this->castValue($value)) {
+			return false;
+		}
+		return true;
+	}
+}
