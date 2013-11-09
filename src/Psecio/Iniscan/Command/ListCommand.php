df85614946643d816677c033c9ceb43e02e222fa
enygma <enygma@phpdeveloper.org>
2013-11-08 19:53:55 -0500

    adding # of tests to list, reformatting output a bit

--- src/Psecio/Iniscan/Command/ListCommand.php
+++ src/Psecio/Iniscan/Command/ListCommand.php
@@ -28,14 +28,16 @@ class ListCommand extends Command
         $scan = new \Psecio\Iniscan\Scan();
         $rules = $scan->getRules();
 
+        $ruleCount = 0;
         $output->writeLn("\n<fg=yellow>Current tests:</fg=yellow>");
         foreach ($rules as $section => $ruleSet) {
             $output->writeLn('<info>'.$section.'</info>');
             foreach ($ruleSet as $rule) {
+                $ruleCount++;
                 $ruleKey = (isset($rule->test->key)) ? $rule->test->key : '[custom]';
-                $output->writeLn(str_pad($ruleKey, 30).'| '.$rule->description);
+                $output->writeLn('   '.str_pad($ruleKey, 30).'| '.$rule->description);
             }
-            $output->writeLn("\n");
         }
+        $output->writeLn("\n<info>".$ruleCount.' tests</info>');
     }
 }
