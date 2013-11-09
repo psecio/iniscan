532530b3f2eb1f683b30a53da9526a60d338d540
Eric Hogue <eric@erichogue.ca>
2013-11-08 19:58:59 -0500

    Refactored the Operation tests to use a data provider

--- tests/Psecio/Iniscan/OperationTest.php
+++ tests/Psecio/Iniscan/OperationTest.php
@@ -6,68 +6,35 @@ require_once 'OperationStub.php';
 
 class OperationTest extends \PHPUnit_Framework_TestCase
 {
-	/**
-	 * Test the "casting" of a value to a consistent output
-	 * given a string
-	 *
+
+    /**
+     * Test the "casting of a value to a consistent output.
+     *
 	 * @covers \Psecio\Iniscan\Operation::castValue
-	 */
-    public function testCastValueString()
+     * @dataProvider castDataProvider
+     */
+    public function testCastValue($input, $expectedValue, $expectedType)
     {
-        $input = 'Off';
         $operation = new OperationStub('test');
         $result = $operation->castValue($input);
 
-        $this->assertEquals($result, 0);
-        $this->assertEquals(gettype($result), 'integer');
+        $this->assertEquals($result, $expectedValue);
+        $this->assertEquals(gettype($result), $expectedType);
     }
 
     /**
-	 * Test the "casting" of a value to a consistent output
-	 * given an integer
-	 *
-	 * @covers \Psecio\Iniscan\Operation::castValue
-	 */
-    public function testCastValueInteger()
+     * Data provider for the cast tests
+     */
+    public function castDataProvider()
     {
-    	$input = 1;
-        $operation = new OperationStub('test');
-        $result = $operation->castValue($input);
-
-        $this->assertEquals($result, 1);
-        $this->assertEquals(gettype($result), 'integer');
-    }
-
-	/**
-	 * Test the "casting" of a value to a consistent output
-	 * given number 0 as a string
-	 *
-	 * @covers \Psecio\Iniscan\Operation::castValue
-	 */
-    public function testCastValueNumberAsString()
-    {
-        $input = '0';
-        $operation = new OperationStub('test');
-        $result = $operation->castValue($input);
-
-        $this->assertEquals($result, 0);
-        $this->assertEquals(gettype($result), 'integer');
-    }
-
-	/**
-	 * Test the "casting" of a value to a consistent output
-	 * given number 1 as a string
-	 *
-	 * @covers \Psecio\Iniscan\Operation::castValue
-	 */
-    public function testCastValueNumber1AsString()
-    {
-        $input = '1';
-        $operation = new OperationStub('test');
-        $result = $operation->castValue($input);
-
-        $this->assertEquals($result, 1);
-        $this->assertEquals(gettype($result), 'integer');
+        return array(
+            array('Off', 0, 'integer'),
+            array('On', 1, 'integer'),
+            array(0, 0, 'integer'),
+            array(1, 1, 'integer'),
+            array('0', 0, 'integer'),
+            array('1', 1, 'integer'),
+        );
     }
 
     /**
