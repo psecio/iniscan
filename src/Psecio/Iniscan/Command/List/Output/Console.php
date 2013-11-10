7fad423b006214a0113874665645a3a620e475f0
enygma <enygma@phpdeveloper.org>
2013-11-10 13:36:34 -0500

    adding List output options

---
+++ src/Psecio/Iniscan/Command/List/Output/Console.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Psecio\Iniscan\Command\List\Output;
+
+class Console extends \Psecio\Iniscan\Command\Output
+{
+    public function render($rules)
+    {
+    	$output = $this->getOutput();
+    	$ruleCount = 0;
+
+    	$output->writeLn("\n<fg=yellow>Current tests:</fg=yellow>");
+        foreach ($rules as $section => $ruleSet) {
+            $output->writeLn('<info>'.$section.'</info>');
+            foreach ($ruleSet as $rule) {
+                $ruleCount++;
+                $ruleKey = (isset($rule->test->key)) ? $rule->test->key : '[custom]';
+                $output->writeLn('   '.str_pad($ruleKey, 30).'| '.$rule->description);
+            }
+        }
+        $output->writeLn("\n<info>".$ruleCount.' tests</info>');
+    }
+}
