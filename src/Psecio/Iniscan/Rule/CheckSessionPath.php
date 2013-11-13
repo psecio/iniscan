15a3a87d86c1ec5455bd944915ae3e87d1b30752
Chris Cornutt <ccornutt@mashery.com>
2013-11-13 14:05:31 -0600

    Adding check on sesssion path for memcache handling

--- src/Psecio/Iniscan/Rule/CheckSessionPath.php
+++ src/Psecio/Iniscan/Rule/CheckSessionPath.php
@@ -13,7 +13,33 @@ class CheckSessionPath extends \Psecio\Iniscan\Rule
 		$this->setTest(array('key' => 'session.save_path'));
 	}
 
+	/**
+	 * Perform the evluation of the rule
+	 *
+	 * @param array $ini Configuration settings (from php.ini)
+	 * @return boolean Pass/fail of evaluation
+	 */
 	public function evaluate(array $ini)
+	{
+		// See which type we're working with
+		switch(strtolower($ini['Session']['session.save_handler'])) {
+			case 'file':
+				return $this->evaluateFile($ini);
+				break;
+			case 'memcache':
+				return $this->evaluateMemcache($ini);
+				break;
+		}
+		return false;
+	}
+
+	/**
+	 * Evaluate the session handling for file-based systems
+	 *
+	 * @param array $ini Configuration settings (from php.ini)
+	 * @return boolean Pass/fail of evaluation
+	 */
+	protected function evaluateFile(array $ini)
 	{
 		if (!isset($ini['Session']['session.save_path'])) {
 			$this->setDescription('Path not set, default (/tmp) is world writeable');
@@ -30,5 +56,16 @@ class CheckSessionPath extends \Psecio\Iniscan\Rule
 			$this->pass();
 			return true;
 		}
+	}
+
+	/**
+	 * Check the memcache session storage settings
+	 *
+	 * @param array $ini Configuration settings (from php.ini)
+	 * @return boolean Pass/fail of evaluation;
+	 */
+	protected function evaluateMemcache(array $ini)
+	{
+		return true;
 	}
 }
