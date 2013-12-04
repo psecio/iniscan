174ec23b0554462088e68b50c2e089c61b35c275
enygma <enygma@phpdeveloper.org>
2013-12-03 21:43:12 -0600

    adding Rule for checking session entropy path

---
+++ src/Psecio/Iniscan/Rule/CheckSessionEntropyPath.php
@@ -0,0 +1,30 @@
+<?php
+namespace Psecio\Iniscan\Rule;
+
+/**
+ * Custom operation - Checks to see if the maximum
+ * 	post size is too large
+ */
+class CheckSessionEntropyPath extends \Psecio\Iniscan\Rule
+{
+	public function __construct($config, $section)
+	{
+		parent::__construct($config, $section);
+		$this->setTest(array('key' => 'session.entropy_file'));
+	}
+
+	public function evaluate(array $ini)
+	{
+		$entropyFile = $ini['Session']['session.entropy_file'];
+
+		// If the version is less than 5.4.0
+		if (version_compare(PHP_VERSION, '5.4.0', '<') === true) {
+			if ($entropyFile === '') {
+				$this->fail();
+				return false;
+			}
+		}
+		$this->pass();
+		return true;
+	}
+}
