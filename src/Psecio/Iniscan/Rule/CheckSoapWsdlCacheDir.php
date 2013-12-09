698d286446afb3969e9ab23bbbc758c3a7b5eb6b
enygma <enygma@phpdeveloper.org>
2013-12-08 20:25:00 -0600

    updating cachedir test for new variable names and handling

--- src/Psecio/Iniscan/Rule/CheckSoapWsdlCacheDir.php
+++ src/Psecio/Iniscan/Rule/CheckSoapWsdlCacheDir.php
@@ -27,10 +27,16 @@ class CheckSoapWsdlCacheDir extends \Psecio\Iniscan\Rule
 	 */
 	public function evaluate(array $ini)
 	{
-		// Resolve paths to deal with symbolic links
-		$openBasedir = realpath($this->findValue('session.open_basedir', $ini));
+		$openBasedir = $this->castValue($this->findValue('open_basedir', $ini));
+
+		if ($openBasedir === 0) {
+			return true;
+		} else {
+			$openBasedir = realpath($openBasedir);
+		}
+
 		$wsdlCacheDir = realpath($this->findValue('soap.wsdl_cache_dir', $ini));
-		$wsdlCacheEndabled = $this->findValue('soap.wsdl_cache_enabled', $ini);
+		$wsdlCacheEnabled = $this->castValue($this->findValue('soap.wsdl_cache_enabled', $ini));
 
 		// We only care about PHP before 5.3.22 and 5.4.x before 5.4.13
 		if (version_compare($this->getVersion(), '5.3.22', '>=') || version_compare($this->getVersion(), '5.4.13', '>='))
@@ -39,19 +45,19 @@ class CheckSoapWsdlCacheDir extends \Psecio\Iniscan\Rule
 		}
 
 		// This only matters is an open_basedir is set and soap.wsdl_cache_enabled is enabled
-		if (empty($openBasedir) || empty($wsdlCacheEndabled))
+		if (empty($openBasedir) || $wsdlCacheEnabled === 0)
 		{
 			return true;
 		}
 
 		// Make sure the folders are still valid
-		if (!$openBasedir)
+		if ($openBasedir === false)
 		{
 			$this->setDescription('The open_basedir did not resolve to a valid directory');
 			$this->fail();
 			return false;
 		}
-		if (!$wsdlCacheDir)
+		if ($wsdlCacheDir === false)
 		{
 			$this->setDescription('The SOAP WSDL cache directory did not resolve to a valid directory');
 			$this->fail();
@@ -59,7 +65,7 @@ class CheckSoapWsdlCacheDir extends \Psecio\Iniscan\Rule
 		}
 
 		// Ensure that the WSDL cache directory is inside the base directory
-		if (strpos($wsdlCacheDir, $openBasedir) !== 0)
+		if (strpos($wsdlCacheDir, $openBasedir) === false)
 		{
 			$this->setDescription('soap.wsdl_cache_dir is not inside of open_basedir which allows the creation of cached SOAP WSDL files in an arbitrary directory');
 			$this->fail();
