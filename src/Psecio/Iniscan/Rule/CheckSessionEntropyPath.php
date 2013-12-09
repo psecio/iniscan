a97dfb6c955d79720a6bfb3cf2a74f01a07eabf9
enygma <enygma@phpdeveloper.org>
2013-12-08 21:03:51 -0600

    removing commented code

--- src/Psecio/Iniscan/Rule/CheckSessionEntropyPath.php
+++ src/Psecio/Iniscan/Rule/CheckSessionEntropyPath.php
@@ -15,7 +15,6 @@ class CheckSessionEntropyPath extends \Psecio\Iniscan\Rule
 
 	public function evaluate(array $ini)
 	{
-		// $entropyFile = $ini['Session']['session.entropy_file'];
 		$entropyFile = $this->findValue('session.entropy_file', $ini);
 
 		// If the version is less than 5.4.0
