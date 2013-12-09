126f08f76d4c0a230438710d1425490962d3bf07
enygma <enygma@phpdeveloper.org>
2013-12-08 20:21:22 -0600

    Adding castValues, castPowers to Rule class

--- src/Psecio/Iniscan/Rule.php
+++ src/Psecio/Iniscan/Rule.php
@@ -298,8 +298,48 @@ class Rule
 			$value = ini_get($path);
 			$ini[$section][$path] = $value;
 		}
-
 		return $value;
+	}
+
+	/**
+	 * Cast the values from php.ini to a standard format
+	 *
+	 * @param mixed $value php.ini setting value
+	 * @return mixed "Casted" result
+	 */
+	public function castValue($value)
+	{
+		if ($value === 'Off' || $value === '' || $value === 0 || $value === '0') {
+			$casted = 0;
+		} elseif ($value === 'On' || $value === '1' || $value === 1) {
+			$casted = 1;
+		} else {
+			$casted = $value;
+		}
+
+		$casted = $this->castPowers($casted);
+
+		return $casted;
+	}
+
+	/**
+     * Cast the byte values ending with G, M or K to full integer values
+     *
+     * @param $casted
+     * @internal param $value
+     * @return mixed "Casted" result
+     */
+	public function castPowers ($casted) {
+		$postfixes = array(
+			'K' => 1024,
+			'M' => 1024 * 1024,
+			'G' => 1024 * 1024 * 1024,
+		);
+		$matches = array();
+		if (preg_match('/^([0-9]+)([' . implode('', array_keys($postfixes)) . '])$/', $casted, $matches)) {
+			$casted = $matches[1] * $postfixes[$matches[2]];
+		}
+		return $casted;
 	}
 
     /**
