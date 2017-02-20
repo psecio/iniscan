0827f1fd7feedd78b9927b780d7432cac0623da4
Andreas Katzig <andreas.katzig@ottonova.de>
2017-02-20 16:24:16 +0100

    - Extracted html output filename creation into dedicated function. - Added Unit Test for html output filename creation.

--- src/Psecio/Iniscan/Command/ScanCommand/Output/Html.php
+++ src/Psecio/Iniscan/Command/ScanCommand/Output/Html.php
@@ -9,26 +9,12 @@ class Html extends \Psecio\Iniscan\Command\Output
 	 */
 	public function render($results)
 	{
-		$output = $this->getOption('output');
-
-		$outputDir = dirname($output);
-		$outputFilename = basename($output);
-
-		// Find out if a filename has been given in the output argument. If not, create the default output filename.
-		// this checks for *.htm*
-		if (strpos($outputFilename, ".htm") === FALSE) {
-			// To keep backward compatibility, use the value of
-			//the given output argument as the directory name.
-			$outputDir = $output;
-			$outputFilename = 'iniscan-output-'.date('Ymd').'.html';
-		}
+		$outputFilePath = $this->getOutputFilename($this->getOption('output'));
 
 		// Check if the configured output directory exists. If not, create it.
-		if (!is_dir($outputDir)) {
-			mkdir($outputDir, 0777, true);
+		if (!is_dir(dirname($outputFilePath))) {
+			mkdir(dirname($outputFilePath), 0777, true);
 		}
-
-		$outputFilePath = $outputDir . DIRECTORY_SEPARATOR . $outputFilename;
 
 		// read in the template file
 		$template = file_get_contents(__DIR__.'/../Templates/html.html');
@@ -54,11 +40,52 @@ class Html extends \Psecio\Iniscan\Command\Output
 			$values['results'] .= $resultHtml;
 		}
 
-		if (is_writable($outputDir)) {
+		if (is_writable(dirname($outputFilePath))) {
 			foreach ($values as $key => $value) {
 				$template = str_replace('{{'.$key.'}}', $value, $template);
 			}
 			file_put_contents($outputFilePath, $template);
 		}
+	}
+
+	/**
+	 * Returns path and filename for the output file.
+	 * @param $output The output path configured via its argument
+	 */
+	public function getOutputFilename($output)
+	{
+		// default behaviour / backwards compatibility
+		$outputDir = $output;
+		$outputFilename = $this->getDefaultOutputFilename();
+
+		// Find out if a .htm(l) filename has been given in the output argument.
+		if ($this->endsWith($output, ".htm") || $this->endsWith($output, ".html")) {
+			$outputDir = dirname($output);
+			$outputFilename = basename($output);
+		}
+
+		return $outputDir . DIRECTORY_SEPARATOR . $outputFilename;
+	}
+
+	public function getDefaultOutputFilename()
+	{
+		return 'iniscan-output-'.date('Ymd').'.html';
+	}
+
+	/**
+	 * Return true if $haystack ends with $needle.
+	 * Source: http://stackoverflow.com/a/834355
+	 * @param $haystack
+	 * @param $needle
+	 * @return bool
+	 */
+	private function endsWith($haystack, $needle)
+	{
+		$length = strlen($needle);
+		if ($length == 0) {
+			return true;
+		}
+
+		return (substr($haystack, -$length) === $needle);
 	}
 }
