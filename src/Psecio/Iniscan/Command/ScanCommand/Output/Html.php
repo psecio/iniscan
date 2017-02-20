c4584092b39b6d9081bf8511844847af66234843
Andreas Katzig <andreas.katzig@ottonova.de>
2017-02-20 10:48:28 +0100

    - Adding the possibility to define a filename for generated HTML files via the '--output' argument. - Create the defined output folder for HTML format if it doesn't exist.

--- src/Psecio/Iniscan/Command/ScanCommand/Output/Html.php
+++ src/Psecio/Iniscan/Command/ScanCommand/Output/Html.php
@@ -4,13 +4,31 @@ namespace Psecio\Iniscan\Command\ScanCommand\Output;
 
 class Html extends \Psecio\Iniscan\Command\Output
 {
-    /**
-     * @param \Psecio\Iniscan\Rule[] $results
-     */
-    public function render($results)
-    {
-        $output = $this->getOutput();
+	/**
+	 * @param \Psecio\Iniscan\Rule[] $results
+	 */
+	public function render($results)
+	{
 		$output = $this->getOption('output');
+
+		$outputDir = dirname($output);
+		$outputFilename = basename($output);
+
+		// Find out if a filename has been given in the output argument. If not, create the default output filename.
+		// this checks for *.htm*
+		if (strpos($outputFilename, ".htm") === FALSE) {
+			// To keep backward compatibility, use the value of
+			//the given output argument as the directory name.
+			$outputDir = $output;
+			$outputFilename = 'iniscan-output-'.date('Ymd').'.html';
+		}
+
+		// Check if the configured output directory exists. If not, create it.
+		if (!is_dir($outputDir)) {
+			mkdir($outputDir, 0777, true);
+		}
+
+		$outputFilePath = $outputDir . DIRECTORY_SEPARATOR . $outputFilename;
 
 		// read in the template file
 		$template = file_get_contents(__DIR__.'/../Templates/html.html');
@@ -24,7 +42,7 @@ class Html extends \Psecio\Iniscan\Command\Output
 			$pass = ($result->getStatus() === true) ? 'pass' : 'fail';
 
 			if ($result->getStatus() === null) {
-				$pass = 'warn';
+			    $pass = 'warn';
 			}
 
 			$resultHtml = '<div class="result '.$pass.'">';
@@ -36,12 +54,11 @@ class Html extends \Psecio\Iniscan\Command\Output
 			$values['results'] .= $resultHtml;
 		}
 
-		if (is_writable($output)) {
-			$output .= '/iniscan-output-'.date('Ymd').'.html';
+		if (is_writable($outputDir)) {
 			foreach ($values as $key => $value) {
 				$template = str_replace('{{'.$key.'}}', $value, $template);
 			}
-			file_put_contents($output, $template);
+			file_put_contents($outputFilePath, $template);
 		}
 	}
 }
